c29ce9827eeba46e8abe2ed7b4f3898154a628c7
Miguel Mota <miguelmota2@gmail.com>
2018-05-26 16:10:54 -0700

    remove cached

--- node_modules/install/scripts/docs.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd $(dirname $0)/..
-docco install.js
-cd docs
-mv install.html index.html
-cd ..
--- node_modules/install/scripts/prepublish.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cd $(dirname $0)/..
-
-uglifyjs install.js -c -m > install.min.js
