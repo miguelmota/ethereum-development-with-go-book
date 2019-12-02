8e15ba1c94b97a91ad73059d3deda5adbcf45fe5
Arber Avdullahu <arber_919@live.com>
2019-12-02 21:39:20 +0100

    indentation

--- code/blocks.go
+++ code/blocks.go
@@ -29,7 +29,7 @@ func main() {
 	}
 
 	fmt.Println(block.Number().Uint64())     // 5671744
-	fmt.Println(block.Time())       // 1527211625
+	fmt.Println(block.Time())       	 // 1527211625
 	fmt.Println(block.Difficulty().Uint64()) // 3217000136609065
 	fmt.Println(block.Hash().Hex())          // 0x9e8751ebb5069389b855bba72d94902cc385042661498a415979b7b6ee9ba4b9
 	fmt.Println(len(block.Transactions()))   // 144
